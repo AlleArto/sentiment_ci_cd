a43a31cde2d2115aec3e2c417e7637cf9366bdf6
alessandro.artoni <alessandro.artoni@otconsulting.com>
2025-07-27 17:26:51 +0200

    Update dataset name and change evaluation dataset to validation set

--- src/config.py
+++ src/config.py
@@ -6,11 +6,11 @@ from pathlib import Path
 BASE_MODEL = os.getenv("BASE_MODEL", "cardiffnlp/twitter-roberta-base-sentiment-latest")
 HF_REPO_ID = os.getenv("HF_REPO_ID", "AlleArto/twitter-sentiment-roberta-ft")
 
-DATASET_NAME = os.getenv("DATASET_NAME", "igor-freik/tweet_sentiment_extraction_300")
+DATASET_NAME = os.getenv("DATASET_NAME", "Ocelot02/tweet-sentiment-ita-eng")
 
 # Addestramento
 EPOCHS = int(os.getenv("EPOCHS", 3))
-BATCH = int(os.getenv("BATCH", 32))
+BATCH = int(os.getenv("BATCH", 64))
 LR = float(os.getenv("LR", 2e-5))
 
 # Percorsi
