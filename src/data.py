4016037bb98326e349128f668917cf04e29c5ebb
alessandro.artoni <alessandro.artoni@otconsulting.com>
2025-07-27 18:57:06 +0200

    Reduce training epochs from 10 to 5 and decrease dataset sample sizes for training, validation, and test sets

--- src/data.py
+++ src/data.py
@@ -4,9 +4,9 @@ from config import BASE_MODEL, DATASET_NAME
 
 def load_tokenized_dataset():
     ds = load_dataset(DATASET_NAME)
-    ds["train"] = ds["train"].shuffle(seed=0).select(range(400))
-    ds["validation"] = ds["validation"].shuffle(seed=0).select(range(100))
-    ds["test"] = ds["test"].shuffle(seed=0).select(range(100))
+    ds["train"] = ds["train"].shuffle(seed=0).select(range(200))
+    ds["validation"] = ds["validation"].shuffle(seed=0).select(range(50))
+    ds["test"] = ds["test"].shuffle(seed=0).select(range(50))
     tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL, add_prefix_space=True)
 
     def tok(batch):
