57612f6abd91fd542524cd4659aa58eb169384b1
alessandro.artoni <alessandro.artoni@otconsulting.com>
2025-07-27 16:57:59 +0200

    Update dataset name for sentiment extraction

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BASE_MODEL = os.getenv("BASE_MODEL", "cardiffnlp/twitter-roberta-base-sentiment-latest")
 HF_REPO_ID = os.getenv("HF_REPO_ID", "AlleArto/twitter-sentiment-roberta-ft")
 
-DATASET_NAME = os.getenv("DATASET_NAME", "SetFit/tweet_eval-sentiment")
+DATASET_NAME = os.getenv("DATASET_NAME", "mteb/tweet_sentiment_extraction")
 
 # Addestramento
 EPOCHS = int(os.getenv("EPOCHS", 3))
