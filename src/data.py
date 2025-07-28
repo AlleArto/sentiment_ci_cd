55a289107a484ce83da1c9a22afce781ab9f91e0
alessandro.artoni <alessandro.artoni@otconsulting.com>
2025-07-28 11:10:33 +0200

    Reduce training dataset size by adjusting training samples from 200 to 150

--- src/data.py
+++ src/data.py
@@ -4,7 +4,7 @@ from config import BASE_MODEL, DATASET_NAME
 
 def load_tokenized_dataset():
     ds = load_dataset(DATASET_NAME)
-    ds["train"] = ds["train"].shuffle(seed=0).select(range(200))
+    ds["train"] = ds["train"].shuffle(seed=0).select(range(150))
     ds["validation"] = ds["validation"].shuffle(seed=0).select(range(50))
     ds["test"] = ds["test"].shuffle(seed=0).select(range(50))
     tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL, add_prefix_space=True)
